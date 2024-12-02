42010ee439a408835ba5d46ae7d26d808903af42
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-12-02 18:41:58 +0600

    create or post data on database

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
-require('dotenv').config()
-const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require('cors')
-const app = express()
-const port =process.env.PORT || 5500
+require("dotenv").config();
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5500;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6avkk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
-  try {
-   
-    
-  } finally {
   
-  }
+    const carCollection = client.db("carsDB").collection("cars");
+
+    // create car
+    app.post("/cars", async (req, res) => {
+      const newCar = req.body;
+      const result = await carCollection.insertOne(newCar);
+      res.send(result);
+    });
+
+    // 
+ 
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res)=>{
-    res.send('Car Server is running.......')
-})
-app.listen(port, ()=>{
-    console.log(`Car Server is Running on port ${port}`)
-})
+app.get("/", (req, res) => {
+  res.send("Car Server is running.......");
+});
+app.listen(port, () => {
+  console.log(`Car Server is Running on port ${port}`);
+});

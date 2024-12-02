ad1a1f1b111e6f26904aebcfcb14b436fc3fb28a
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-12-02 20:09:28 +0600

    get car from database for update

--- index.js
+++ index.js
@@ -41,6 +41,14 @@ async function run() {
         const id = req.params.id;
         const query = {_id: new ObjectId(id)}
         const result = await carCollection.deleteOne(query)
+        res.send(result)    
+    })
+
+    // get car for update
+    app.get('/cars/:id', async (req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await carCollection.findOne(query)
         res.send(result)
     })
  
